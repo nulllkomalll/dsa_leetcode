d619bfce51781b0daaa9191c3f27a83018fb48a1
Komal <nulllkomalll@gmail.com>
2026-06-16 11:36:18 +0530

    Implement sortedSquares function to square sorted array

---
+++ arrays/two_pointer/977. squares of sorted array.cpp
@@ -0,0 +1,25 @@
+// abs = absolute value = number ka sign hata do 
+
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n);
+        int left =0;
+        int right= n-1;
+        int idx = n-1;
+
+        while(left <= right){
+            if (abs(nums[left])> abs(nums[right])){
+                ans[idx]= nums[left] * nums[left];
+                left++ ;
+            }
+            else{
+                ans[idx] = nums[right] * nums[right];
+                right-- ;
+            }
+            idx--;
+        }
+        return ans;
+    }
+};
