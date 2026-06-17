ad89aecc6d2620deb6b6886cdc40654080550926
Komal <nulllkomalll@gmail.com>
2026-06-17 12:21:50 +0530

    Add sortColors function for sorting three colors
    Implement Dutch National Flag Algorithm to sort colors.

---
+++ arrays/Dutch National Flag Algorithm/sort colors/code.cpp
@@ -0,0 +1,25 @@
+//jab 3 categories hai, aur unko ek sqence mein lagna ho toh ye use krna hai
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0;
+        int mid=0;
+        int high= nums.size()-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+            else{
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+        }
+    }
+};
