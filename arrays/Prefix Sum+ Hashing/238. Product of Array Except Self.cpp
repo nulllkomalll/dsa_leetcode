91fa12780b0ba5662d56f5aa13cbf39d0e73fe8a
Komal <nulllkomalll@gmail.com>
2026-06-23 11:06:26 +0530

    Implement productExceptSelf function in C++

---
+++ arrays/Prefix Sum+ Hashing/238. Product of Array Except Self.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+            int n= nums.size();
+            vector<int> ans(n);
+
+            int prefix=1;
+            for(int i=0;i<n; i++){
+                ans[i]= prefix;
+                prefix *= nums[i];
+            }
+
+            int suffix=1;
+            for(int j=n-1; j>=0; j--){
+                ans[j] *= suffix;
+                suffix*= nums[j];
+            }
+        return ans;
+    }
+};
